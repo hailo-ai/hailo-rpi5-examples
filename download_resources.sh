5c8a8f203ca0fae6f323507427914d223d29cd91
Gilad Nahor <giladn@hailo.ai>
2024-05-30 13:50:02 +0300

    Updated Documentation, added multiple network support for detection app

--- download_resources.sh
+++ download_resources.sh
@@ -12,5 +12,7 @@ wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolo
 wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l_pi.hef -P ./resources
 wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolox_s_leaky_h8l_mz.hef -P ./resources
 wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov6n.hef -P ./resources
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_h8l.hef -P ./resources
+
 # download video file to ./resources
 wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/video/detection0.mp4 -P ./resources
