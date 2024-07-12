b8abfbe396ad5b3c1372f9e700076e9fb4b0a0b2
JP Meijers <jp.meijers@canonical.com>
2024-07-12 11:09:00 +0200

    Remove autofocus forced enable

--- basic_pipelines/instance_segmentation.py
+++ basic_pipelines/instance_segmentation.py
@@ -112,7 +112,7 @@ class GStreamerInstanceSegmentationApp(GStreamerApp):
 
     def get_pipeline_string(self):
         if (self.source_type == "rpi"):
-            source_element = f"libcamerasrc name=src_0 auto-focus-mode=2 ! "
+            source_element = f"libcamerasrc name=src_0 ! "
             source_element += f"video/x-raw, format={self.network_format}, width=1536, height=864 ! "
             source_element += QUEUE("queue_src_scale")
             source_element += f"videoscale ! "
