f43332e198d2ae1d8169025fa29a46ed0b4a4154
Gilad Nahor <giladn@hailo.ai>
2024-08-25 11:35:14 +0300

    updated version support and setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -2,31 +2,42 @@
 
 # TAPPAS CORE Definitions
 CORE_VENV_NAME="venv_hailo_rpi5_examples"
-CORE_REQUIRED_VERSION=("3.28.2")
+CORE_REQUIRED_VERSION=("3.28.2" "3.29.1")
 
 # TAPPAS Definitions
 TAPPAS_VENV_NAME="hailo_tappas_venv"
-TAPPAS_REQUIRED_VERSION=("3.28.0" "3.28.1" "3.28.2")
+TAPPAS_REQUIRED_VERSION=("3.28.0" "3.28.1" "3.28.2" "3.29.0" "3.29.1")
 
 # Function to check if the script is being sourced
 is_sourced() {
-    [[ "${BASH_SOURCE[0]}" != "$0" ]]
+    if [ -n "$ZSH_VERSION" ]; then
+        [[ -o sourced ]]
+    elif [ -n "$BASH_VERSION" ]; then
+        [[ "${BASH_SOURCE[0]}" != "$0" ]]
+    else
+        echo "Unsupported shell. Please use bash or zsh."
+        return 1
+    fi
 }
 
 # Only proceed if the script is being sourced
 if is_sourced; then
     echo "Setting up the environment..."
 
-    # check if we are working with hailo_tappas or hailo-tappas-core
-    if pkg-config --exists hailo_tappas ; then
+    # Check if we are working with hailo_tappas or hailo-tappas-core
+    if pkg-config --exists hailo_tappas; then
         TAPPAS_CORE=0
-        VENV_NAME=$TAPPAS_VENV_NAME
         REQUIRED_VERSION=("${TAPPAS_REQUIRED_VERSION[@]}")
         echo "Setting up the environment for hailo_tappas..."
         TAPPAS_VERSION=$(pkg-config --modversion hailo_tappas)
         TAPPAS_WORKSPACE=$(pkg-config --variable=tappas_workspace hailo_tappas)
         export TAPPAS_WORKSPACE
         echo "TAPPAS_WORKSPACE set to $TAPPAS_WORKSPACE"
+        if [[ "$TAPPAS_WORKSPACE" == "/local/workspace/tappas" ]]; then
+            VENV_NAME="DOCKER"
+        else
+            VENV_NAME=$TAPPAS_VENV_NAME
+        fi
     else
         TAPPAS_CORE=1
         VENV_NAME=$CORE_VENV_NAME
@@ -34,10 +45,11 @@ if is_sourced; then
         echo "Setting up the environment for hailo-tappas-core..."
         TAPPAS_VERSION=$(pkg-config --modversion hailo-tappas-core)
     fi
+
     # Check if TAPPAS_VERSION is in REQUIRED_VERSION
     version_match=0
     for version in "${REQUIRED_VERSION[@]}"; do
-        if [ "$TAPPAS_VERSION" == "$version" ]; then
+        if [ "$TAPPAS_VERSION" = "$version" ]; then
             version_match=1
             break
         fi
@@ -48,10 +60,11 @@ if is_sourced; then
     else
         echo "TAPPAS_VERSION is ${TAPPAS_VERSION} not in the list of required versions ${REQUIRED_VERSION[*]}."
         return 1
-    fi    
+    fi
+
     if [ $TAPPAS_CORE -eq 1 ]; then
         # Get the directory of the current script
-        SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+        SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" &> /dev/null && pwd)"
         # Check if we are in the defined virtual environment
         if [[ "$VIRTUAL_ENV" == *"$VENV_NAME"* ]]; then
             echo "You are in the $VENV_NAME virtual environment."
@@ -69,19 +82,23 @@ if is_sourced; then
         fi
         TAPPAS_POST_PROC_DIR=$(pkg-config --variable=tappas_postproc_lib_dir hailo-tappas-core)
     else
-        # Check if we are in the defined virtual environment
-        if [[ "$VIRTUAL_ENV" == *"$VENV_NAME"* ]]; then
-            echo "You are in the $VENV_NAME virtual environment."
+        if [[ "$VENV_NAME" == "DOCKER" ]]; then
+            echo "Running in DOCKER using default virtualenv"
         else
-            echo "You are not in the $VENV_NAME virtual environment."
-            # Activate TAPPAS virtual environment
-            VENV_PATH="${TAPPAS_WORKSPACE}/hailo_tappas_venv/bin/activate"
-            if [ -f "$VENV_PATH" ]; then
-                echo "Activating virtual environment..."
-                source "$VENV_PATH"
+            # Check if we are in the defined virtual environment
+            if [[ "$VIRTUAL_ENV" == *"$VENV_NAME"* ]]; then
+                echo "You are in the $VENV_NAME virtual environment."
             else
-                echo "Error: Virtual environment not found at $VENV_PATH."
-                return 1
+                echo "You are not in the $VENV_NAME virtual environment."
+                # Activate TAPPAS virtual environment
+                VENV_PATH="${TAPPAS_WORKSPACE}/hailo_tappas_venv/bin/activate"
+                if [ -f "$VENV_PATH" ]; then
+                    echo "Activating virtual environment..."
+                    source "$VENV_PATH"
+                else
+                    echo "Error: Virtual environment not found at $VENV_PATH."
+                    return 1
+                fi
             fi
         fi
         TAPPAS_POST_PROC_DIR="${TAPPAS_WORKSPACE}/apps/h8/gstreamer/libs/post_processes/"
@@ -101,7 +118,7 @@ if is_sourced; then
     # Export the Device Architecture to an environment variable
     export DEVICE_ARCHITECTURE="$device_arch"
     # Print the environment variable to verify
-    echo "Device Architecture is set to: $DEVICE_ARCHITECTURE"
+    echo "DEVICE_ARCHITECTURE is set to: $DEVICE_ARCHITECTURE"
 else
     echo "This script needs to be sourced to correctly set up the environment. Please run '. $(basename "$0")' instead of executing it."
 fi
