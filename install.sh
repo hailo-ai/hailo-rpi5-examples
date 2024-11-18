1da9f974ae55b224b45f7b3471c6be52162fd6fe
OmriA <159430476+OmriAx@users.noreply.github.com>
2024-11-18 14:34:07 +0200

    Updating to the latest release (#65)
    * Adding 3.30 Tappas
    Added CI Fix
    
    * Add Rpi Check Test
    
    * Documentation Update

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Ensure the repository is cloned and you're in the correct directory
-# Uncomment if you haven't cloned it yet
-#git clone https://github.com/hailo-ai/hailo-rpi5-examples.git
-
-# Enter the repository directory
-#cd hailo-rpi5-examples || exit
-
 # Source environment variables and activate virtual environment
 source setup_env.sh
 
@@ -16,12 +9,16 @@ pip install -r requirements.txt
 pip install -r tests/test_resources/requirements.txt
 
 # Install additional system dependencies (if needed)
-sudo apt install -y rapidjson-dev v4l-utils
+sudo apt install -y rapidjson-dev
+
+# Check if the --all flag is provided
+DOWNLOAD_RESOURCES_FLAG=""
+if [[ "$1" == "--all" ]]; then
+    DOWNLOAD_RESOURCES_FLAG="--all"
+fi
 
 # Download resources needed for the pipelines
-./download_resources.sh
+./download_resources.sh $DOWNLOAD_RESOURCES_FLAG
 
 # Optional: Post-process compilation (Only for older TAPPAS versions)
 ./compile_postprocess.sh
-
-
