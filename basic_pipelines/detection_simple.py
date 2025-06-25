67863aa3bf79b36aed660d69fc80fbd3e779077c
OmriAx <omria@hailo.ai>
2025-06-25 04:41:39 +0300

    Update arch to use the new structure

--- basic_pipelines/detection_simple.py
+++ basic_pipelines/detection_simple.py
@@ -4,8 +4,8 @@ import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 import hailo
-from hailo_apps_infra.hailo_apps.hailo_gstreamer.gstreamer_app import app_callback_class
-from hailo_apps_infra.hailo_apps.hailo_pipelines.detection_pipeline_simple import GStreamerDetectionApp
+from hailo_apps.hailo_app_python.core.gstreamer.gstreamer_app import app_callback_class
+from hailo_apps.hailo_app_python.apps.detection.detection_pipeline import GStreamerDetectionApp
 
 # User-defined class to be used in the callback function: Inheritance from the app_callback_class
 class user_app_callback_class(app_callback_class):
